05700ee71685257655139186de4c1913904ae784
tuliohs <tuliohhenriquepaz@gmail.com>
2020-12-13 15:37:44 -0300

    components and passed props

--- src/views/Revision.jsx
+++ src/views/Revision.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useLocation } from 'react-router-dom'
+
+
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -7,7 +10,7 @@ import 'react-vertical-timeline-component/style.min.css';
 const defaultDescr = " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type sp"
 export const currentItem = { id: 3, title: 'Greetings (cumprimentos).', description: defaultDescr, priority: 'little' }
 
-const CardContent = ({ title, description }) => {
+const CardContent = ({ title, description, revision }) => {
     return (
         <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
             <div className="px-4 py-5 flex-auto">
@@ -16,35 +19,62 @@ const CardContent = ({ title, description }) => {
                 </div>
                 <h6 className="text-xl font-semibold">{title}</h6>
                 <p className="mt-2 mb-4 text-gray-600">{description}</p>
+                <div className="divhoverbutton" style={{ width: "20%" }}>
+                    <a className="ahoverbutton" onClick={revision}><span className="spanhoverbutton">New Revision</span></a>
+                </div>
             </div>
         </div>)
 }
 
 const itemRevision = [
-    { date: 'junho 2020', },
-    { date: 'junho 2020', },
-    { date: 'junho 2020', },
-    { date: 'junho 2020', },
-    { date: 'junho 2020', },
-    { date: 'junho 2020', },
-    { date: 'junho 2020', },
+    {
+        idSchedule: 1,
+        revisions: [
+            { date: 'junho 2020', },
+            { date: 'junho 2020', },
+            { date: 'junho 2020', },
+            { date: 'junho 2020', },
+            { date: 'junho 2020', },
+            { date: 'junho 2020', },
+            { date: 'junho 2020', }],
+    },
+    {
+        idSchedule: 3,
+        revisions: [
+            { date: 'junho 201', },
+            { date: 'junho 201', },
+            { date: 'junho 2021', },]
+    },
+    {
+        idSchedule: 4,
+        revisions: [
+            { date: 'junho 2022', },
+            { date: 'junho 2022', },
+            { date: 'junho 2022', },
+            { date: 'junho 2022', }]
+    }
 ]
-export function TimeLine() {
+
+export function TimeLine({ id }) {
     return (
         <VerticalTimeline>
-            {itemRevision.map(c => (
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    date={c.date}
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                //icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                    <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
-                </VerticalTimelineElement>
-            ))}
+            {itemRevision
+                .filter(c => id ? c.idSchedule == id : c.idSchedule == 3)
+                .map(a =>
+                    a.revisions.map(c => (
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            //contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }  }
+                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                            date={c.date}
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        //icon={<WorkIcon />}
+                        >
+                            <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                            <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+                        </VerticalTimelineElement>
+                    )))}
             <VerticalTimelineElement
                 iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
             //icon={<StarIcon />}
@@ -52,12 +82,30 @@ export function TimeLine() {
         </VerticalTimeline>)
 }
 
+
+export function EmptyRevision() {
+    return (
+        <div className="px-4 py-5 flex-auto ">
+            <h6 className="text-xl font-semibold">The history of your revisions will appear here</h6>
+        </div>
+    )
+}
+
 export default function Revision() {
+    let location = useLocation();
+    const defaultItem = {
+
+    }
+
     return (
         <>
             <div className="flex flex-wrap" style={{ justifyContent: "center" }}>
-                <CardContent title={currentItem.title} description={currentItem.description} />
-                <TimeLine />
+                <CardContent title={location.state?.title} description={location.state?.description} />
+                {
+                    location.state?.id ?
+                        <EmptyRevision /> :
+                        <TimeLine id={location.state?.id} />
+                }
 
             </div>
         </>
--- src/views/Schedule.jsx
+++ src/views/Schedule.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
 
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+import 'components/Buttons/buttonHover.css'
 // components
+import ModalSmall from 'components/Modals/ModalSmall'
 
 const defaultDescr = " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type sp"
 export const defaultStepItems = [
     {
+        idCategory: 1,
         category: "Level One",
         items: [
             { id: 1, title: 'Personal pronouns', description: defaultDescr, priority: 'little' },
@@ -13,50 +19,70 @@ export const defaultStepItems = [
         ]
     },
     {
+        idCategory: 2,
         category: "Level Two",
         items: [
-            { id: 1, title: 'Personal pronouns', description: defaultDescr, priority: 'little' },
-            { id: 2, title: 'This, that, these e those demonstrative pronouns.', description: defaultDescr, priority: 'little' },
-            { id: 3, title: 'Greetings (cumprimentos).', description: defaultDescr, priority: 'little' },
-            { id: 4, title: 'Verbo to be', description: defaultDescr, priority: 'little' },
-            { id: 5, title: 'Adjetivos: Comuns e Demonstrativos', description: defaultDescr, priority: 'little' },
-            { id: 6, title: 'Advérbios de Frequência', description: defaultDescr, priority: 'little' },
-            { id: 7, title: 'Comparativo e Superlativo', description: defaultDescr, priority: 'little' },
-            { id: 8, title: '‘To be going to’ – frases básicas', description: defaultDescr, priority: 'little' },
-            { id: 9, title: 'Quantificadores: ‘How much’, ‘how many’ e ‘very’ – frases básicas', description: defaultDescr, priority: 'little' },
-            { id: 10, title: 'Substantivos Incontáveis mais Comuns', description: defaultDescr, priority: 'little' },
+            { id: 4, title: 'Personal pronouns', description: defaultDescr, priority: 'little' },
+            { id: 5, title: 'This, that, these e those demonstrative pronouns.', description: defaultDescr, priority: 'little' },
+            { id: 6, title: 'Greetings (cumprimentos).', description: defaultDescr, priority: 'little' },
+            { id: 7, title: 'Verbo to be', description: defaultDescr, priority: 'little' },
+            { id: 8, title: 'Adjetivos: Comuns e Demonstrativos', description: defaultDescr, priority: 'little' },
+            { id: 9, title: 'Advérbios de Frequência', description: defaultDescr, priority: 'little' },
+            { id: 10, title: 'Comparativo e Superlativo', description: defaultDescr, priority: 'little' },
+            { id: 11, title: '‘To be going to’ – frases básicas', description: defaultDescr, priority: 'little' },
+            { id: 12, title: 'Quantificadores: ‘How much’, ‘how many’ e ‘very’ – frases básicas', description: defaultDescr, priority: 'little' },
+            { id: 14, title: 'Substantivos Incontáveis mais Comuns', description: defaultDescr, priority: 'little' },
         ]
     },
     {
+        idCategory: 3,
         category: "Level Three",
         items: [
-            { id: 1, title: 'Personal pronouns', description: defaultDescr, priority: 'little' },
-            { id: 2, title: 'This, that, these e those demonstrative pronouns.', description: defaultDescr, priority: 'little' },
-            { id: 3, title: 'Greetings (cumprimentos).', description: defaultDescr, priority: 'little' },
-            { id: 4, title: 'Verbo to be', description: defaultDescr, priority: 'little' },
-            { id: 5, title: 'Adjetivos: Comuns e Demonstrativos', description: defaultDescr, priority: 'little' },
+            { id: 14, title: 'Personal pronouns', description: defaultDescr, priority: 'little' },
+            { id: 15, title: 'This, that, these e those demonstrative pronouns.', description: defaultDescr, priority: 'little' },
+            { id: 16, title: 'Greetings (cumprimentos).', description: defaultDescr, priority: 'little' },
+            { id: 17, title: 'Verbo to be', description: defaultDescr, priority: 'little' },
+            { id: 18, title: 'Adjetivos: Comuns e Demonstrativos', description: defaultDescr, priority: 'little' },
         ]
     }
 
 ]
 
-const CardContent = ({ title, description }) => {
+const CardContent = ({ id, title, description, revision }) => {
     return (
+
         <div className="w-full md:w-4/12 px-4 text-center">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
-                        <i className="fas fa-retweet"></i>
+                    <div className="flex">
+
+                        <div style={{ width: "80%", marginLeft: "10%" }}>
+                            <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
+                                <i className="fas fa-retweet"></i>
+                            </div></div>
+
+                        <Link className="relative w-auto pl-4 flex-initial"
+                            to={{ pathname: "revision", state: { id: id, description: description, title: title, } }}
+                        >
+                            < ExitToAppIcon />
+                        </Link>
                     </div>
                     <h6 className="text-xl font-semibold">{title}</h6>
                     <p className="mt-2 mb-4 text-gray-600">{description}</p>
+                    {/*<button class="button-rgb" type="button">NEW REVISION</button>*/}
+                    <div className="divhoverbutton">
+                        <a className="ahoverbutton" onClick={revision}><span className="spanhoverbutton">New Revision</span></a>
+                    </div>
                 </div>
             </div>
-        </div>)
+        </div>
+    )
 }
 
-
 export default function Schedule() {
+
+    const [showModal, setShowModal] = useState(false)
+    const actionModal = () => setShowModal(!showModal)
     return (
         <>
             <div className="flex flex-wrap" style={{ justifyContent: "center" }}>
@@ -68,8 +94,8 @@ export default function Schedule() {
                             {/* Divider */}
                             <hr className="my-6 md:min-w-full" />
                         </div>
-                        {c.items.map(x => <CardContent title={x.title} description={x.description} />)}
-
+                        {c.items.map(x => <CardContent id={x.id} title={x.title} description={x.description} revision={actionModal} />)}
+                        <ModalSmall setShowModal={setShowModal} showModal={showModal} />
                     </>
                 ))}
             </div>
